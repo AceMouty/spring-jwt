52b845dd71db52293987cb6a0602191f87514044
Kyle Guerrero <kyleguerrero85@gmail.com>
2021-10-25 12:18:50 -0500

    chore: created config for web security server

---
+++ src/main/java/io/acemouty/springjwt/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package io.acemouty.springjwt.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+  // beans created with overrides
+  private final UserDetailsService userDetailsService;
+  private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception
+  {
+    // tell spring how to look for users
+    auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception
+  {
+    super.configure(http);
+  }
+}
